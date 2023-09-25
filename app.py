cf10208b35a27a97e7a7e4a467766dfb61934962
pradeep saini <pradeepsaini384@gmail.com>
2023-09-25 21:54:48 +0530

    Webiste Merged

--- app.py
+++ app.py
@@ -5,6 +5,29 @@ from flask import Flask,render_template ,Response,redirect,request,flash,url_for
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+#---------------- website Directories ------------------
+@app.route('/')
+def website_home():
+    return render_template('/website/index.html')
+
+@app.route('/Website_Contact')
+def Website_Contact():
+    return render_template('/website/contact.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('/website/login.html')
+
+@app.route('/user/signup')
+def user_signup():
+    return render_template('/website/signup.html')
+@app.route('/appointment')
+def appointment():
+    return render_template('/website/appointment.html')
+
+
+
 
 @app.route('/User')
 def home():
@@ -16,4 +39,4 @@ def user_page(Name):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
